4b7560e21ac95c68d61ac7003971abb078ed5e08
eunjooNine <eunjoo.nine@gmail.com>
2024-08-09 21:10:09 +0900

    test11

--- deployment.sh
+++ deployment.sh
@@ -3,11 +3,13 @@ spring_pid=`ps -ef | grep demo | grep -v grep | awk -F " " '{print $2}'`
 if [ $spring_pid ]
 then
    echo "실행중인 백엔드 프로세스 중료하는 중..."
-   sudo kill -9 $spring_pid
+
 else
    echo "실행중인 백엔드 프로세스가 없습니다."
  fi
 
- echo "스프링 프로젝트 실행 중.."
- nohup java -jar /home/ubuntu/spring/build/libs/demo-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+echo "스프링 프로젝트 실행 중.."
+
+sudo kill -9 $spring_pid
+nohup java -jar /home/ubuntu/spring/build/libs/demo-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 
