ed46c51492a234f513ef7c6565330974437850e8
eunjooNine <eunjoo.nine@gmail.com>
2024-08-09 15:19:06 +0900

    test1

--- src/main/java/com/example/demo/VersionController.java
+++ src/main/java/com/example/demo/VersionController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController sta
+@RestController
 public class VersionController {
     @RequestMapping(method = RequestMethod.GET, value = "/version")
     public ResponseEntity<String> getVersion() {
-        return ResponseEntity.ok("Hello World");
+        return ResponseEntity.ok("Hello World ggu");
     }
 }
