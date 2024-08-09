ba197d875cd0b9e22e7518b448844a0b2a9c0c17
eunjooNine <eunjoo.nine@gmail.com>
2024-08-09 15:00:28 +0900

    기본파일

---
+++ src/main/java/com/example/demo/VersionController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController sta
+public class VersionController {
+    @RequestMapping(method = RequestMethod.GET, value = "/version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
